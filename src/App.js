a8cacdec4e1d2b8e93a5fd1f8b7b4e6427bbd51a
abhishek <abhishekmazumder69@gmail.com>
2020-06-17 20:26:51 +0530

    404 page Not Found functionality implemented

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import {BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Header from './components/layout/Header';
 import AddContact from './components/AddContact'
 import ContactList from './components/ContactList';
-import About from './components/pages/About'
+import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from './context';
 
@@ -15,9 +16,16 @@ class App extends Component {
 					<div className="app">
 						<Header branding="Contact Manager" />
 						<div className="container">
-							<Route exact path="/" component={ContactList} />
-							<Route exact path="/contact/add" component={AddContact} />
-							<Route exact path="/about" component={About} />
+							<Switch>
+								<Route exact path="/" component={ContactList} />
+								<Route
+									exact
+									path="/contact/add"
+									component={AddContact}
+								/>
+								<Route exact path="/about" component={About} />
+								<Route component={NotFound}/>
+							</Switch>
 						</div>
 					</div>
 				</Router>
